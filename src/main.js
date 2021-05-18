825ad817b42271639ac1b081008a0b6a056e8435
Prabhjot Sodhi <sodhiprabhjot23@gmail.com>
2021-05-18 16:16:11 +1200

    Styling Navigation Complete

--- src/main.js
+++ src/main.js
@@ -9,9 +9,13 @@ window.onload = function () {
 
 	const menu_btn = document.querySelector('.hamburger');
 	const mobile_menu = document.querySelector('.mobile-nav');
+	const logo_img = document.querySelector('.logo').firstChild;
+	const logo = document.querySelector('.logo');
 
 	menu_btn.addEventListener('click', function () {
 		menu_btn.classList.toggle('is-active');
 		mobile_menu.classList.toggle('is-active');
+		logo_img.classList.toggle('is-active');
+		logo.style.color = 'white'
 	});
 }

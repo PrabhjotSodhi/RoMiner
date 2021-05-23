08c0c8c4e732742fb7ae6db050ae8fba9a92542a
Prabhjot Sodhi <sodhiprabhjot23@gmail.com>
2021-05-23 16:39:33 +1200

    Hero setup needs media quries

--- src/main.js
+++ src/main.js
@@ -1,21 +1,25 @@
-window.onload = function () {
-	window.addEventListener('scroll', function (e) {
-		if (window.pageYOffset > 100) {
-			document.querySelector("header").classList.add('is-scrolling');
-		} else {
-			document.querySelector("header").classList.remove('is-scrolling');
-		}
-	});
+window.onload = function() {
+    window.addEventListener('scroll', function(e) {
+        if (window.pageYOffset > 100) {
+            document.querySelector("header").classList.add('is-scrolling');
+        } else {
+            document.querySelector("header").classList.remove('is-scrolling');
+        }
+    });
 
-	const menu_btn = document.querySelector('.hamburger');
-	const mobile_menu = document.querySelector('.mobile-nav');
-	const logo_img = document.querySelector('.logo').firstChild;
-	const logo = document.querySelector('.logo');
+    const menu_btn = document.querySelector('.hamburger');
+    const mobile_menu = document.querySelector('.mobile-nav');
+    const logo = document.querySelector('.logo');
 
-	menu_btn.addEventListener('click', function () {
-		menu_btn.classList.toggle('is-active');
-		mobile_menu.classList.toggle('is-active');
-		logo_img.classList.toggle('is-active');
-		logo.style.color = 'white'
-	});
+    menu_btn.addEventListener('click', function() {
+        menu_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
+    });
+    mobile_menu.addEventListener('transitionstart', () => {
+        if (Array.from(mobile_menu.classList.values()).indexOf('is-active') != -1) {
+            setTimeout(() => logo.classList.add('is-active'), 0.15 * 1000);
+        } else {
+            setTimeout(() => logo.classList.remove('is-active'), 0.05 * 1000);
+        }
+    })
 }
